adbff2180db3bd647022db18134233a95fb4fd06
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-04-22 20:53:07 +0200

    feat(api): implement currentUser and logout routes

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const router = express.Router();
+const auth = require('../middlewares/auth');
+const { User } = require('../models');
 const { UserRouter, NoteRouter } = require('./index');
+const router = express.Router();
 
 router.use('/user', UserRouter);
 router.use('/note', NoteRouter);
+
+router.get('/currentUser', auth, async (req, res) => {
+  const user = await User.findById(req.ctx.userId);
+  if (!user)
+    return res.sendStatus(404);
+  res.json(user);
+});
+
+router.get('/logout', auth, async (req, res) => {
+  res.clearCookie('x-access-token').sendStatus(200);
+});
 
 module.exports = router;
