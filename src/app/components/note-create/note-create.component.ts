d184bbed0b88289510fb6f1283b9b4ffca235636
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-04-26 01:56:06 +0200

    feat(app/components): implement NoteCreateComponent

--- src/app/components/note-create/note-create.component.ts
+++ src/app/components/note-create/note-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-note-create',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-create.component.scss']
 })
 export default class NoteCreateComponent implements OnInit {
+  public closed = true;
 
   constructor() { }
 
   ngOnInit(): void { }
 
+  autoResizeBody(ta: any) {
+    ta.style.height = 'auto';
+    ta.style.height = ta.scrollHeight + 'px';
+  }
+
+  onBodyFocus() {
+    this.closed = false;
+  }
+
+  onSubmit(f: NgForm) {
+    // TODO create note and clear form
+    console.log(f.value);
+    this.closed = true;
+  }
 }
