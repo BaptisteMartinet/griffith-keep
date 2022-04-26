012fe40cda78774ad73926fe6195db9ec368ea7b
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-04-26 13:11:57 +0200

    feat(app/views): reimplement auth service in login view

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -24,16 +24,12 @@ export default class LoginComponent implements OnInit {
   }
 
   public async submitLogin() {
-    const loginRes = await fetch(`${environment.API_URI}/user/login`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        email: 'baptiste.martinet@student.griffith.ie',
-        password: 'test',
-      }),
+    const loginStatus = await this.authService.login({
+      email: 'baptiste.martinet@student.griffith.ie',
+      password: 'test',
     });
-    console.log(loginRes.ok);
+    if (loginStatus)
+      window.location.replace('/');
   }
 
 }
