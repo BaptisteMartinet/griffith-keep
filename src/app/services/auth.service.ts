dd322d3a79a0bef822c8c8474c49e49f99ab94fc
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-04-26 13:10:27 +0200

    feat(app/services): implement register/login/logout routes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,6 +8,18 @@ interface UserT {
   email: string,
 }
 
+interface LoginArgsT {
+  email: string,
+  password: string,
+}
+
+interface RegisterArgsT {
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -25,5 +37,29 @@ export default class AuthService {
       return this.userSubject.next(null);
     const userJson = await userRes.json();
     this.userSubject.next(userJson);
+  }
+
+  async register(args: RegisterArgsT): Promise<boolean> {
+    const registerRes = await fetch(`${environment.API_URI}/user/register`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(args),
+    });
+    return registerRes.ok;
+  }
+
+  async login(args: LoginArgsT): Promise<boolean> {
+    const loginRes = await fetch(`${environment.API_URI}/user/login`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(args),
+    });
+    return loginRes.ok;
+  }
+
+  async logout(): Promise<boolean> {
+    const logoutRes = await fetch(`${environment.API_URI}/logout`, { credentials: 'include' });
+    return logoutRes.ok;
   }
 }
