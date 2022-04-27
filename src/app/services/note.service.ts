af2c10d7b36c2885ba3ad284cb2bd045d98ac161
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-04-27 21:10:36 +0200

    feat(app/services): implement NoteService

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { UserT } from './auth.service';
 
-interface NoteT {
+export interface NoteT {
   author: UserT,
   assigned: Array<UserT>,
   title: string,
@@ -10,20 +12,70 @@ interface NoteT {
   completionDate: Date,
 }
 
+export interface NoteCreateArgsT {
+  assigned?: Array<string>,
+  title?: string,
+  body: string,
+  pinned?: boolean,
+  completionDate?: Date,
+}
+
+export interface NoteUpdateArgsT {
+  assigned?: Array<string>,
+  title?: string,
+  body?: string,
+  pinned?: boolean,
+  completionDate?: Date,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export default class NoteService {
   public notes: Array<NoteT> = [];
+  public notesSubject = new Subject<Array<NoteT>>();
 
-  constructor() { }
+  constructor() {
+    this.notesSubject.subscribe(notes => { this.notes = notes; });
+  }
 
-  async loadNotes() { }
+  async loadNotes() {
+    const notesRes = await fetch(`${environment.API_URI}/note`, { credentials: 'include' });
+    if (!notesRes.ok)
+      return;
+    const notes: Array<NoteT> = await notesRes.json();
+    this.notesSubject.next(notes);
+  }
 
-  async createNote() { }
+  async createNote(args: NoteCreateArgsT) {
+    const noteRes = await fetch(`${environment.API_URI}/note`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(args),
+    });
+    if (!noteRes.ok)
+      return;
+    await this.loadNotes(); // Service realoads all the notes when a new one is created (bad practice)
+  }
 
-  async updateNote() { }
+  async updateNote(id: string, args: NoteUpdateArgsT) {
+    const res = await fetch(`${environment.API_URI}/note/${id}`, {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(args),
+    });
+  }
 
-  async deleteNote() { }
+  async deleteNote(noteId: string) {
+    const res = await fetch(`${environment.API_URI}/note/${noteId}`, {
+      method: 'DEL',
+      credentials: 'include',
+    });
+    if (!res.ok)
+      return;
+    await this.loadNotes();
+  }
 
 }
