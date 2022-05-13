d3acc834837480475cbe267e8a9e7bffe50e4f81
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-05-13 19:54:21 +0200

    feat(components/navbar): implement search bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from 'src/app/services';
+import { AuthService, NoteService } from 'src/app/services';
 import type { UserT } from 'src/app/services/auth.service';
 
 @Component({
@@ -11,14 +11,16 @@ import type { UserT } from 'src/app/services/auth.service';
 export default class NavbarComponent implements OnInit {
   public user: UserT | null = null;
 
-  constructor(public authService: AuthService) { }
+  constructor(private authService: AuthService, private noteService: NoteService) { }
 
   ngOnInit(): void {
       this.authService.userObesrvable.subscribe(u => { this.user = u });
   }
 
   onSubmit(f: NgForm) {
-    console.log(f.value.search);
-    // TODO search cards
+    const searchTerm = f.value.search;
+    if (!searchTerm)
+      return;
+    this.noteService.loadNotes(searchTerm);
   }
 }
