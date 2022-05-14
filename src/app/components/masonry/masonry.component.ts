e3525ff523cbdad09f201cbb549509191b466847
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-05-14 12:45:06 +0200

    feat(components/masonry): optimise masonry with querySelectorAll

--- src/app/components/masonry/masonry.component.ts
+++ src/app/components/masonry/masonry.component.ts
@@ -19,7 +19,7 @@ export default class MasonryComponent implements OnInit, AfterViewChecked {
   ngOnInit(): void {
     // Hacky solution to trigger ngAfterViewChecked and refresh the masonry
     // when the window is resized
-    window.addEventListener('resize', () => {});
+    window.addEventListener('resize', () => { });
   }
 
   ngAfterViewChecked(): void {
@@ -29,24 +29,21 @@ export default class MasonryComponent implements OnInit, AfterViewChecked {
   private computeWrappersPositions() {
     if (!this.container.nativeElement.hasChildNodes())
       return;
+    const items = this.container.nativeElement.querySelectorAll<HTMLElement>(`#${this.itemName}`);
+    if (items.length <= 0)
+      return;
     const { width: containerWidth } = this.container.nativeElement.getBoundingClientRect();
     const nbColumns = Math.floor(containerWidth / this.columnWidth);
     const offsetX = containerWidth % this.columnWidth * 0.5;
-    const columnsHeights = new Array<number>(nbColumns);
-    columnsHeights.fill(0);
-    let idx = 0;
+    const columnsHeights = new Array<number>(nbColumns).fill(0);
 
-    this.container.nativeElement.childNodes.forEach((node) => {
-      const nodeElement: HTMLElement = node as HTMLElement;
-      if (nodeElement.id !== this.itemName)
-        return;
+    items.forEach((item, idx) => {
       const columnIdx = idx % nbColumns;
       const posX = offsetX + columnIdx * this.columnWidth;
       const posY = columnsHeights[columnIdx];
-      nodeElement.style.transform = `translate(${posX}px, ${posY}px)`;
-      const { height } = nodeElement.getBoundingClientRect();
+      item.style.transform = `translate(${posX}px, ${posY}px)`;
+      const { height } = item.getBoundingClientRect();
       columnsHeights[columnIdx] += height;
-      idx++;
     });
 
     const maxColumnHeigth = Math.max(...columnsHeights);
